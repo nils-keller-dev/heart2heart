326b78bbcb4ccc11cd9774f6669e71cc78aaf4b0
tsomic <keller.nils99@gmail.com>
2022-06-15 12:19:01 +0200

    test commit

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
-import WebSocket, { WebSocketServer } from 'ws';
+// import WebSocket, { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+// const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message, { binary: false });
-      }
-    })
-  })
-})
+// wss.on('connection', ws => {
+//   ws.on('message', message => {
+//     wss.clients.forEach((client) => {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(message, { binary: false });
+//       }
+//     })
+//   })
+// })
 
+
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  // process HTTP request. Since we're writing just WebSockets
+  // server we don't have to implement anything.
+});
+server.listen(8080, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+
+  // This is the most important callback for us, we'll handle
+  // all messages from users here.
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      // process WebSocket message
+      // console.log((wsServer))
+      // wsServer.clients.forEach((client) => {
+      // if (client !== ws && client.readyState === WebSocket.OPEN) {
+      //   client.send(message, { binary: false });
+      // }
+    // })
+      console.log(message);
+      connection.sendUTF(message.utf8Data);
+    }
+  });
+
+  connection.on('close', function(connection) {
+    // close user connection
+  });
+});
