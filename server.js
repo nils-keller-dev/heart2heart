9e1bae51b84ad6e4a21b3f79d081faca5d7a9aed
tsomic <keller.nils99@gmail.com>
2022-06-15 13:12:48 +0200

    testing

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-const express = require("express");
 const WebSocket = require("ws");
-const SocketServer = require("ws").Server;
 
-const server = express().listen(8080, () => {
-  console.log("Server started at port 8080");
-});
-
-const wss = new SocketServer({ server });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", (ws) => {
-  console.log("[Server] A client was connected.");
-
-  ws.on("close", () => {
-    console.log("[Server] Client disconnected.");
-  });
-
   ws.on("message", (message) => {
-    console.log("[Server] Received message: %s", message);
-
-    wss.clients.forEach(function each(client) {
+    wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message, { binary: false });
       }
